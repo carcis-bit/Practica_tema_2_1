e58eaed1e7bac1fb5f624daecdd043bbfeb67bdf
Fran Pastor <franpastor9@gmail.com>
2023-02-07 12:08:18 +0100

    Cambio valor de i

--- src/EjemploSuma.java
+++ src/EjemploSuma.java
@@ -6,7 +6,7 @@ public class EjemploSuma {
 	 }
 	
     public static void main(String[] args) {
-      int i = 3;
+      int i = 4;
       int j = 6;
       int x = 5;
       int suma = sumar(i,j,x);
