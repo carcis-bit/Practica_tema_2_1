84c28455eac5a57df73c444beb7ddbf53e22f1ae
carcis-bit <124671396+carcis-bit@users.noreply.github.com>
2023-02-16 13:49:08 +0100

    Prueba

--- src/EjemploSuma.java
+++ src/EjemploSuma.java
@@ -1,19 +1,19 @@
 
 public class EjemploSuma {
-	
-	static int sumar (int a, int b, int c) {
-	return a+b+c;
-	 }
-	
-    public static void main(String[] args) {
-      int i = 14;
-      int j = 7;
-      int x = 5;
-      int suma = sumar(i,j,x);
-      
-      System.out.println("Ejercicio Muestra Suma");  
-      System.out.println("-----------------------------------");
-      System.out.println(i + " + " + j +" = "+suma);
-      System.out.println("Fin Ejercicio");
-    }
+
+	static int sumar(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	public static void main(String[] args) {
+		int i = 14;
+		int j = 7;
+		int x = 5;
+		int suma = sumar(i, j, x);
+
+		System.out.println("Ejercicio Muestra Suma");
+		System.out.println("-----------------------------------");
+		System.out.println(i + " + " + j + " = " + suma);
+		System.out.println("Fin Ejercicio");
+	}
 }
